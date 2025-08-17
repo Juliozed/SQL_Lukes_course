99b8097ff59fa606be391d442536ac7f5a79c605
Julio Zamora <juliozee88@gmail.com>
2025-08-17 00:29:28 -0600

    first commit

---
+++ sql_course2.session.sql
@@ -0,0 +1,14 @@
+/*
+Find the companies that have the most job openings
+- get the total number of jobs postings per company ID
+- Return the total nnumber of jobs with the company name
+*/
+
+
+
+
+
+
+
+
+
